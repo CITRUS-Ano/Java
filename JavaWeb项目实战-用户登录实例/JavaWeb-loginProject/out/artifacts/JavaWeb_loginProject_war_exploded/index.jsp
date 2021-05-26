59c81d54aafc91019e7ea6cce67f1dfad5a15bd4
CITRUS-Ano <zh210929@163.com>
2021-05-27 00:18:05 +0800

    JavaWeb用户登录-第一个小项目

---
+++ JavaWeb项目实战-用户登录实例/JavaWeb-loginProject/out/artifacts/JavaWeb_loginProject_war_exploded/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: AnotherH
+  Date: 2021-05-25
+  Time: 13:23
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+    <h4>欢迎${user.userName}登录</h4>
+  </body>
+</html>
---
+++ JavaWeb项目实战-用户登录实例/JavaWeb-loginProject/src/com/xxxx/entity/User.java
@@ -0,0 +1,43 @@
+package com.xxxx.entity;
+
+/*
+* 用户实体类
+* */
+public class User {
+    private Integer userId; //用户编号
+    private String userName; //用户姓名
+    private String userPwd; //用户密码
+    private Integer userAge; //用户年龄
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserPwd() {
+        return userPwd;
+    }
+
+    public Integer getUserAge() {
+        return userAge;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setUserPwd(String userPwd) {
+        this.userPwd = userPwd;
+    }
+
+    public void setUserAge(Integer userAge) {
+        this.userAge = userAge;
+    }
+}
